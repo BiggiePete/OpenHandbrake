03ac910989c927e11fd93a1007dd5306481b1eeb
BiggiePete <pter.cross222@gmail.com>
2025-11-17 18:18:38 -0500

    remove more of the original functions

--- Firmware/Stm32Duino/src/main.cpp
+++ Firmware/Stm32Duino/src/main.cpp
@@ -1,42 +1,43 @@
 #include "Arduino.h"
 #include "HID-Project.h"
 
-const int pinLed = LED_BUILTIN;
-const int pinButton = 2;
+volatile bool buttonState = false;
 void buttonChange();
 
 void setup()
 {
-  pinMode(pinLed, OUTPUT);
-  pinMode(pinButton, INPUT_PULLUP);
+  pinMode(PB_4, INPUT_ANALOG);
+  attachInterrupt(PA_3, &buttonChange, CHANGE);
 
   // Sends a clean report to the host. This is important on any Arduino type.
 
   Gamepad.begin();
   Gamepad.releaseAll();
-  attachInterrupt(PA_3, &buttonChange, CHANGE);
-  pinMode(PB_4, INPUT_ANALOG);
 }
 
 void loop()
 {
-  if (!digitalRead(pinButton))
+
+  // read in the ADC and get a 16 bit value to write to the x axis
+
+  // Move x/y Axis to a new position (16bit)
+  Gamepad.yAxis((uint16_t)analogRead(PB_4) - 512);
+
+  // Functions above only set the values.
+  // This writes the report to the host.
+  switch (buttonState)
   {
-    digitalWrite(pinLed, HIGH);
-
-    // read in the ADC and get a 16 bit value to write to the x axis
-
-    // Move x/y Axis to a new position (16bit)
-    Gamepad.yAxis((uint16_t)analogRead(PB_4) - 512);
-
-    // Functions above only set the values.
-    // This writes the report to the host.
-    Gamepad.write();
-
-    // Simple debounce
-    delay(100);
-    digitalWrite(pinLed, LOW);
+  case true:
+    Gamepad.press(1);
+    break;
+  case false:
+    Gamepad.release(1);
+    break;
   }
+  Gamepad.write();
+
+  // limit to 10 readings a second
+  delay(100);
 }
 
 // callback for if the button is pressed or changed
@@ -45,7 +46,7 @@ void buttonChange()
   // first lets get the state of the button
   int state = digitalRead(PA_3);
   if (state == HIGH)
-    Gamepad.press(1);
+    buttonState = true;
   else
-    Gamepad.release(1);
+    buttonState = false;
 }
